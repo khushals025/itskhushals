0544773e852c4e9543622638c74024671e4a625e
Khushal Sharma <khushals@buffalo.edu>
2023-12-19 19:18:11 -0500

    Update_03

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '/Users/khushal/Desktop/react-portfolio-website-1/src/assets/RESUME.pdf'
+import CV from '/Users/khushal/Desktop/react-portfolio-website-1/src/assets/Khushal_Sharma_Resume.pdf'
 
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
